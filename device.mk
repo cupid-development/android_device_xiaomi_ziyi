cac9c3c9fe4f48a8356b9ee73c1eb755adf68ae7
dianlujitao <dianlujitao@lineageos.org>
2022-02-02 23:33:07 +0800

    ziyi: Add aw_cali.bin firmware symlink
    Change-Id: Iefc4440f7ea1c45facf52a71cc92c9613e8875fc

--- device.mk
+++ device.mk
@@ -11,6 +11,9 @@ $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 $(call inherit-product, vendor/xiaomi/ziyi/ziyi-vendor.mk)
 
 # Audio
+PRODUCT_PACKAGES += \
+    firmware_aw_cali.bin_symlink
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_diwali_idp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_diwali/mixer_paths_diwali_idp.xml \
     $(LOCAL_PATH)/audio/resourcemanager_diwali_idp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_diwali/resourcemanager_diwali_idp.xml \
