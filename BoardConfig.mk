2048a17207ff444e9620d4c00c0a340c2a9b8cd6
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-20 19:21:09 +0200

    ziyi: Add support for udfps long press sensor
    Change-Id: I7196dce967c9adbc62bb5156ade749c92c7780a1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,5 +12,8 @@ include vendor/xiaomi/ziyi/BoardConfigVendor.mk
 
 DEVICE_PATH := device/xiaomi/ziyi
 
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
+
 # Screen density
 TARGET_SCREEN_DENSITY := 440
