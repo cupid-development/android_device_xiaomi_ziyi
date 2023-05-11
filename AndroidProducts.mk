9ab6265e4e1216c3aa546879664c5d2926d30c89
Jens Reidel <adrian@travitia.xyz>
2023-05-11 17:53:50 +0200

    ziyi: Initial device tree
    Change-Id: I6d195d295b5f94b71c3ff32b21aa33d3af7bf588
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022-2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_ziyi.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_ziyi-user \
+    lineage_ziyi-userdebug \
+    lineage_ziyi-eng
