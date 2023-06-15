7874d351b763d121bff60f2260f28b18cbbd186a
Omkar Chandorkar <gotenksIN@aospa.co>
2023-06-15 14:26:05 +0900

    ziyi: overlay: Enable secure NFC functionality
    Change-Id: I4e9acc7d8ec14237512aadf100d564984aa5a896
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -18,6 +18,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     ApertureResZiyi \
     FrameworksResZiyi \
+    NfcResZiyi \
     SettingsProviderResZiyi \
     SettingsProviderResZiyiCN \
     SettingsResZiyi \
