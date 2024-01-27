e45fc5c3d7fd96449080c3f94762e40490a749fd
Arian <arian.kulmer@web.de>
2024-01-27 11:10:12 +0100

    ziyi: Add regulator, touch, haptics and audio kernel modules
    Change-Id: I33dd7fae555274ddf3dffd530df494b49469ab50

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,6 +12,34 @@ include vendor/xiaomi/ziyi/BoardConfigVendor.mk
 
 DEVICE_PATH := device/xiaomi/ziyi
 
+# Kernel
+device_second_stage_modules := \
+    goodix_fod.ko \
+    qcom_pm8008-regulator.ko \
+    class_dual_role.ko \
+    ln8000_drv.ko \
+    bq27z561_drv.ko \
+    rt17xx_drv.ko \
+    syv690d_drv.ko \
+    battmngr_common.ko \
+    xm_battery.ko \
+    xm_battmngr_iio.ko \
+    xm_battmngr.ko \
+    xm_charger.ko \
+    xm_pd_mngr.ko \
+    rt_pd_manager.ko \
+    tcpci_drv.ko \
+    tcpc_rt1711h.ko \
+    synaptics_tcm_core_module.ko \
+    synaptics_tcm_reflash.ko \
+    synaptics_tcm_spi.ko \
+    aw862xx_haptic.ko
+
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(device_second_stage_modules)
+BOARD_VENDOR_KERNEL_MODULES_LOAD += $(device_second_stage_modules)
+
+BOOT_KERNEL_MODULES += $(device_second_stage_modules)
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
