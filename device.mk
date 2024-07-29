48c59c53e25f2fcdae65a1a0da3ab45304fb177b
Jens Reidel <adrian@travitia.xyz>
2024-07-29 20:47:58 +0200

    ziyi: Set HWC-specific properties
    Change-Id: I111af006fca4fffa6695119663c422ccd7edab8d
    Co-authored-by: danielml <daniel@danielml.dev>
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- device.mk
+++ device.mk
@@ -10,10 +10,6 @@ $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/ziyi/ziyi-vendor.mk)
 
-# Init scripts
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/init.ziyi.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.ziyi.rc
-
 # Overlay
 PRODUCT_PACKAGES += \
     ApertureResZiyi \
@@ -29,3 +25,12 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+
+# System properties
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/properties/build_CN.prop:$(TARGET_COPY_OUT_ODM)/etc/build_CN.prop \
+    $(LOCAL_PATH)/properties/build_GL.prop:$(TARGET_COPY_OUT_ODM)/etc/build_GL.prop
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/properties/build_CN.prop:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/odm/etc/build_CN.prop \
+    $(LOCAL_PATH)/properties/build_GL.prop:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/odm/etc/build_GL.prop
