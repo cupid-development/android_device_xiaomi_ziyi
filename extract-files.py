04ee1a690e86ea53357d5408b11f226e137dde49
Michael Bestas <mkbestas@lineageos.org>
2024-11-05 23:16:08 +0200

    ziyi: ELF checks are enabled by default
    Change-Id: I5740f4c8d359831b4cb794912c5d650f55c33f17

--- extract-files.py
+++ extract-files.py
@@ -65,7 +65,6 @@ module = ExtractUtilsModule(
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
-    check_elf=True,
     add_firmware_proprietary_file=True,
 )
 
