bbf9ee9219a89dbfcd53e6fe3945bb0269633f4f
Dmitry <42121541+dkpost3@users.noreply.github.com>
2025-05-19 18:59:24 +0300

    adds  euicc overlay (e-sim)
    for  work aads this commits :
    https://github.com/Evolution-X-Devices/hardware_xiaomi/commit/e63544240a0943ad416ab99c44d2524ed56512c1
    https://github.com/Evolution-X-Devices/hardware_xiaomi/commit/5542e53940c58654a1760c51c34b3705190aae57
    https://github.com/Evolution-X-Devices/hardware_xiaomi/commit/6e22a96df556bf573ef8c72f6f901b70c0c70d1b

--- device.mk
+++ device.mk
@@ -10,18 +10,25 @@ $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/ziyi/ziyi-vendor.mk)
 
-# Audio
-PRODUCT_PACKAGES += \
+# MIUI CAMERA
+$(call inherit-product, device/xiaomi/miuicamera-ziyi/device.mk)
+$(call inherit-product, device/xiaomi/dolby/device.mk)
+
+RODUCT_PACKAGES += \
     firmware_aw_cali.bin_symlink
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_diwali_idp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_diwali/mixer_paths_diwali_idp.xml \
     $(LOCAL_PATH)/audio/resourcemanager_diwali_idp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_diwali/resourcemanager_diwali_idp.xml \
-    $(LOCAL_PATH)/audio/usecaseKvManager.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usecaseKvManager.xml
+    $(LOCAL_PATH)/audio/usecaseKvManager.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usecaseKvManager.xml# Audio
+
+# Light
+$(call soong_config_set,xiaomi_sm8450_sensor_notifier,extension_lib,//device/xiaomi/ziyi:libsensor-notifier-ext-light)
 
 # Overlay
 PRODUCT_PACKAGES += \
     ApertureResZiyi \
+    FrameworkOverlayZiyiEsim \
     FrameworksResZiyi \
     NfcResZiyi \
     SettingsProviderResZiyi \
@@ -30,6 +37,34 @@ PRODUCT_PACKAGES += \
     SystemUIResZiyi \
     WifiResZiyi \
     WifiResZiyiCN
+
+PRODUCT_PACKAGES += \
+    XiaomiEuicc
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
+# Logging
+SPAMMY_LOG_TAGS := \
+    MiStcImpl \
+    SDM \
+    SDM-histogram \
+    SRE \
+    SensorService \
+    WifiHAL \
+    cnss-daemon \
+    libcitsensorservice@2.0-impl \
+    libsensor-displayalgo \
+    libsensor-parseRGB \
+    libsensor-ssccalapi \
+    sensors \
+    vendor.qti.hardware.display.composer-service \
+    vendor.xiaomi.sensor.citsensorservice@2.0-service
+
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+PRODUCT_VENDOR_PROPERTIES += \
+    $(foreach tag,$(SPAMMY_LOG_TAGS),log.tag.$(tag)=E)
+endif
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
