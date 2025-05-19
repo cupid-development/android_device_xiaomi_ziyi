79cb66a533de188908a816950662a7b2fd9e1da9
Dmitry <42121541+dkpost3@users.noreply.github.com>
2025-05-19 19:09:17 +0300

    fixed a typo that caused the build to fail.

--- light/RawLightNotifier.cpp
+++ light/RawLightNotifier.cpp
@@ -90,7 +90,7 @@ void RawLightNotifier::notify() {
             continue;
         }
 
-        struct disp_event_resp* response = parseDispEvent(disp_fd_.get());
+        std::shared_ptr<disp_event_resp> response = parseDispEvent(disp_fd_.get());
         if (response == nullptr) {
             continue;
         }
